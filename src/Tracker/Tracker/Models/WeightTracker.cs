a5e219adcf090a90757d3aa35820913caf564d28
cohen990 <dancohen447@gmail.com>
2014-05-24 10:56:40 +0100

    Created project - Tracker

---
+++ src/Tracker/Tracker/Models/WeightTracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tracker.Models
+{
+    public class WeightTracker
+    {
+        public WeightTracker()
+        {
+            var WeighIns = new Dictionary<WeighIn, DateTime>();
+        }
+
+        public Dictionary<WeighIn, DateTime> WeighIns { get; set; }
+    }
+}
